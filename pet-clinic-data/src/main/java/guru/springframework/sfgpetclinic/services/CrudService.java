95e97f7b7ce411f9cd93e8a3af3c2d2676c0827c
pieceofchum <pieceofchum@yahoo.com>
2020-07-11 18:23:17 -0400

    Implemented map based Service. closes #6

--- pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/services/CrudService.java
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/services/CrudService.java
@@ -5,7 +5,7 @@ import java.util.Set;
 public interface CrudService<T, ID> {
     Set<T> findAll();
     T findById(ID id);
-    T save(T object);
+    T save(ID id, T object);
     void delete(T object);
     void deleteById(ID id);
 }
