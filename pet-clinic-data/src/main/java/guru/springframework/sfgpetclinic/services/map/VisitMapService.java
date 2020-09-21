b3b98602471838a5d1ef11b3130ea21d131a0fe3
David Raymond <pieceofchum@yahoo.com>
2020-09-20 20:44:02 -0400

    Update Profiles to use Spring Data JPA Services. Closes #48

--- pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/services/map/VisitMapService.java
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/services/map/VisitMapService.java
@@ -2,9 +2,13 @@ package guru.springframework.sfgpetclinic.services.map;
 
 import guru.springframework.sfgpetclinic.model.Visit;
 import guru.springframework.sfgpetclinic.services.VisitService;
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Service;
 
 import java.util.Set;
 
+@Service
+@Profile({"default", "map"})
 public class VisitMapService extends AbstractMapService<Visit, Long> implements VisitService {
     @Override
     public Set<Visit> findAll() {
@@ -27,7 +31,7 @@ public class VisitMapService extends AbstractMapService<Visit, Long> implements 
                 || visit.getPet().getOwner().getId() == null) {
             throw new RuntimeException("Invalid Visit");
         }
-        
+
         return super.save(visit);
     }
 
