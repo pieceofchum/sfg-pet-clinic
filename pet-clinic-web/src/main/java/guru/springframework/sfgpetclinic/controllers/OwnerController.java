f9a7e1b00b5838015b6e67008365868695a6ea89
David Raymond <pieceofchum@yahoo.com>
2020-10-01 00:30:29 -0400

    Created Spring MVC Tests for Owner Controller. Closes #52

--- pet-clinic-web/src/main/java/guru/springframework/sfgpetclinic/controllers/OwnerController.java
+++ pet-clinic-web/src/main/java/guru/springframework/sfgpetclinic/controllers/OwnerController.java
@@ -14,7 +14,7 @@ public class OwnerController {
         this.ownerService = ownerService;
     }
 
-    @RequestMapping({"", "/index", "/templates/vets/index.html"})
+    @RequestMapping({"", "/index", "/index.html"})
     public String listOwners(Model model) {
         model.addAttribute("owners", ownerService.findAll());
         return "owners/index";
